4989acf77c088de70564a9483e63c8de54983179
Yaswanth <yaswanthk722@gmail.com>
2016-09-19 23:18:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.get('/:articleName', function (req, res) {
   res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/about', function (req, res) {
+app.get('/about.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'about.html'));
 });
 
