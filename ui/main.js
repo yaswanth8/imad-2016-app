2bf449c52b6f5165c4efac8c3f16b8d37917640f
Yaswanth <yaswanthk722@gmail.com>
2016-09-20 23:49:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,4 @@
 console.log('Loaded!');
+// change the element of main-text div
+var element= document.getElementById('main-text');
+element.InnerHTML='Hi!! this is my web page and I\'m Yaswanth from Vijayawada';
