bd13b2bbf25942a49ce8f16dd4f5ffd57ffde929
Yaswanth <yaswanthk722@gmail.com>
2016-09-21 00:16:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,15 @@ console.log('Loaded!');
 
 var element=document.getElementById('main-text');
 element.innerHTML='Hi I am Yaswanth This is my webapp and Im from vijayawada ';
+
+// move the image
+
+var img=document.getElementById('madi');
+
+function moveRight(){
+    marginLeft= marginLeft+10;
+    img.style.marginLeft+'px';
+}
+img.onclick= function(){
+    var interval= setInterval(moveRight,100);
+}
