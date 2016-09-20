af256e68a7005243b2d98601dc08bbcaae0977f6
Yaswanth <yaswanthk722@gmail.com>
2016-09-21 00:10:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,4 +2,4 @@ console.log('Loaded!');
 // change the element of main-text div
 
 var element=document.getElementById('main-text');
-element.innerHTML='Hi I am Yaswanth';
+element.innerHTML='Hi I am Yaswanth This is my webapp and Im from vijayawada ';
