0d9757629c4d898e9e72e350a5aae5026cf39866
Yaswanth <yaswanthk722@gmail.com>
2016-09-20 22:40:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -126,6 +126,10 @@ app.get('/ui/ganesh.jpeg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'ganesh.jpeg'));
 });
 
+app.get('/ui/DSC_0113.JPG', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'DSC_0113.JPG'));
+});
+
 app.get('/ui/group1.jpeg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'group1.jpeg'));
 });
