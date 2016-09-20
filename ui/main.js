f747dda156b9c91c2b3fd92285689ea35dfc4347
Yaswanth <yaswanthk722@gmail.com>
2016-09-21 00:21:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,7 +11,7 @@ var img=document.getElementById('madi');
 var marginLeft=0;
 function moveRight(){
     marginLeft= marginLeft + 10;
-    img.style.marginLeft+'px';
+    img.style.marginLeft= marginLeft +'px';
 }
 img.onclick= function(){
     var interval= setInterval(moveRight,100);
