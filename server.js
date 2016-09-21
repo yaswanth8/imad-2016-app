35188ad7af5a7756bc9469ae00a35d019345eaff
Yaswanth <yaswanthk722@gmail.com>
2016-09-21 20:29:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ var articles= {
     head: '<a href="/">HOME</a>',
     heading:'Article one',
     date:'19-09-2016',
-    content: ` <p> Hi this is first .Hi this is first article.Hi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first article new using var and functions</p>`
+    content: ` <p> Hi this is first .Hi this is first article.Hi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first article new using var and functions</p>`,
+     foot: '<p></p>'
     
 },
 'article-two':{
@@ -19,7 +20,8 @@ var articles= {
        head: '<a href="/">HOME</a>',
     heading:'Article two',
     date:'20-09-2016',
-    content: ` <p> This is the second article  Hi this is first .Hi this is first article.Hi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first article new using var and functions</p>`
+    content: ` <p> This is the second article  Hi this is first .Hi this is first article.Hi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first article new using var and functions</p>`,
+     foot: '<p></p>'
   
 },
 'article-three':{
@@ -28,7 +30,8 @@ var articles= {
     heading:'Article three',
     date:'22-09-2016',
     content: ` <p>This is third article
-    Hi this is first .Hi this is first article.Hi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first article new using var and functions</p>`
+    Hi this is first .Hi this is first article.Hi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first articleHi this is first article new using var and functions</p>`,
+     foot: '<p></p>'
   
 },
  'article-four':{title:'About | Yaswanth',
@@ -37,7 +40,8 @@ var articles= {
         date:'20-09-2016',
         content: `<p> We are still in the basic stage</p>
               <p> This web application is soon going to update</p>
-              <p> Hope you guys co-operate with us make this Big</p>`
+              <p> Hope you guys co-operate with us make this Big</p>`,
+              foot: '<p></p>'
 },
 
 'article-five':{title:'Contact Us | Yaswanth',
@@ -61,7 +65,8 @@ var articles= {
             <hr/>
             <img src="/ui/group1.jpeg" class="img-high"/>
             
-             Team Members</p>`
+             Team Members</p>`,
+              foot: '<p></p>'
 }
 
 };
