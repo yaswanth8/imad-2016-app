5c98117cd12e58a3cc3620157c0d73b3a38900bd
Yaswanth <yaswanthk722@gmail.com>
2016-09-22 23:13:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -138,8 +138,8 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/yaswanth.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'yaswanth.jpg'));
 });
 
 app.get('/ui/fb.png', function (req, res) {
