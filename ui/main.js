c1d66435d9bd43dbc907dfdd241de44285226ab8
Yaswanth <yaswanthk722@gmail.com>
2016-09-24 11:15:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -34,7 +34,7 @@ submit.onclick = function () {
     var list='';
     for (var i= 0; i< names.lenght; i++){
         
-        list== '<li>' + names[i]+ '</li>';
+        list== '<li>' + names[i] + '</li>';
         
     }
     var ul= document.getElementById('namelist');
