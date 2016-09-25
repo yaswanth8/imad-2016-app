70576bf63c8e4ec6a1d4ce0452017c0b328705a7
Yaswanth <yaswanthk722@gmail.com>
2016-09-25 13:41:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,9 @@
 console.log('Loaded!');
-// create a request object
+// counter code
 var counter= 0;
 var button= document.getElementById('counter');
 button.onclick= function () {
-    // make a request to the counter endpoint
+    // create a request object
     var request= new XMLHttpRequest();
     // capture the response and store it in a variable
      request.onreadystatechange= function(){
@@ -16,28 +16,47 @@ button.onclick= function () {
              }
          }
      };
-    
+
     //make a request
-    request.open('GET','http://yaswanth8.imad.hasura-app.io/counter',true);
+    request.open('GET','/counter',true);
     request.send(null);
 };
 // Submit name
-var nameInput= document.getElementById('name');
-var name= nameInput.value;
+
 var submit= document.getElementById('submit_btn');
 
 submit.onclick = function () {
-// make a request to the serve and send name
-  
-// capture the list of the names and render it as a list
-    var names= ['name1','name2','name3','name4'];
-    var list='';
-    for (var i= 0; i< names.length; i++) {
-        
-        list += '<li>' + names[i] + '</li>';
-        
-    }
-    var ul= document.getElementById('namelist');
-    ul.innerHTML= list;
-    
+
+  // create a request object
+  var request= new XMLHttpRequest();
+  // capture the response and store it in a variable
+   request.onreadystatechange= function(){
+       if(request.readyState===XMLHttpRequest.DONE){
+           //Take some action
+           if(request.status===200){
+             // capture the list of the names and render it as a list
+                 var names= request.responseText;
+                 names= JSON.parse(names);
+                 var list='';
+                 for (var i=0; i< names.length; i++) {
+
+                     list += '<li>' + names[i] + '</li>';
+
+                 }
+                 var ul= document.getElementById('namelist');
+                 ul.innerHTML= list;
+           }
+       }
+   };
+
+
+   var nameInput= document.getElementById('name');
+   var name= nameInput.value;
+  //make a request
+  request.open('GET','/submit-name?name='+ name,true);
+  request.send(null);
+
+
+
+
 };
