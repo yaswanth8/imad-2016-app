5807f50d028e49a5cdb6bb44887815cecb27d25f
Yaswanth <yaswanthk722@gmail.com>
2016-10-24 22:33:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var config={
     database:'yaswanth8',
     host:'db.imad.hasura-app.io',
     port:'5432',
-    password:process.env.DB_PASSWORD;
+    password: process.env.DB_PASSWORD
     
 }
 var app = express();
