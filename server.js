31a12b5a760f769aae2c13903cd9edb22d74b2f7
Yaswanth <yaswanthk722@gmail.com>
2016-10-24 22:31:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var Pool= require('pg').Pool;
+var config={
+    user:'yaswanth8',
+    database:'yaswanth8',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD;
+    
+}
 var app = express();
 app.use(morgan('combined'));
 
@@ -117,6 +125,25 @@ function createTemplate(data){
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+var pool= new Pool(config);
+
+app.get('/test-db', function (req, res) {
+ 
+ // make a select request
+ // return a response with results
+ pool:query('SELECT * FROM article',function(err,result){
+     if(err){
+         res.status(500).send(err.toString());
+     }
+     else{
+         res.send(JSON.stringify(result));
+     }
+ });
+ 
+});
+
+
 var counter= 0;
 app.get('/counter', function (req, res) {
     counter= counter + 1;
