da9e13121c37209cfff619fd32ec76b08a786691
Yaswanth <yaswanthk722@gmail.com>
2016-11-08 18:46:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -161,12 +161,25 @@ app.get('/submit-name/',function(req,res){  // URL ://submit-name?namename=xxxx
 });
 
 
-app.get('/:articleName', function (req, res) {
-    var articleName=req.params.articleName;
+app.get('/articles/:articleName', function (req, res) {
+    
      // articleName = article-one
     // articles[articleName] == {} content object for article one
-  res.send(createTemplate(articles[articleName]));
-});
+    pool.query("SELECT * FROM article WHERE title = '"+req.params.articleName+"'",function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else{
+            if(result.rows.length===0){
+                res.status(404).send('article not found');
+            }
+            else{
+                var articleData= result.rows[0];
+                res.send(createTemplate(articleData));
+            }
+        }
+    });
+  });
 
 
 
