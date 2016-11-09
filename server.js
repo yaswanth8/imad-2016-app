62bfa1f897d0c4bc76090c26c9da78f764b741ed
Yaswanth <yaswanthk722@gmail.com>
2016-11-09 23:32:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ function hash(input, salt){
     
     var hashed= crypto.pbkdf2Sync(input, salt,100000, 512, 'sha512');
     
-    return hashed.toString('hex');
+    return ["pbkdf2","10000",salt,hashed.toString('hex')].join('$');
 }
 
 
