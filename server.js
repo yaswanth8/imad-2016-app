2c14fb6bac7838c865974577543f9f465a9db0fe
Yaswanth <yaswanthk722@gmail.com>
2016-11-10 00:37:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ app.post('/login',function(req,res){
    
   
   
-   pool.query('SELECT * FROM "user" WHERE username=$1',[username],function(err,result){
+   pool.query('SELECT * FROM "user" WHERE username= $1',[username],function(err,result){
         if(err){
          res.status(500).send(err.toString());
      }
