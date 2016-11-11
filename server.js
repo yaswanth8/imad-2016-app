0157aa0b91d6461ddab39718a219afae1c5e776b
Yaswanth <yaswanthk722@gmail.com>
2016-11-11 23:44:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.get('/hash/:input',function(req,res){
 });
 
 
-app.post('/create-user',function(req,res){
+app.get('/create-user',function(req,res){
    // username and password
    var username= req.body.username;
    var password= req.body.password;
