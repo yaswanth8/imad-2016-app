789f91687bfe494c9116ad483b62e789477a58d1
Yaswanth <yaswanthk722@gmail.com>
2016-11-12 22:53:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -237,6 +237,22 @@ app.get('/articles/:articleName', function (req, res) {
   });
 });
 
+app.get('/pages/:pageName', function (req, res) {
+  // SELECT * FROM article WHERE title = '\'; DELETE WHERE a = \'asdf'
+  pool.query("SELECT * FROM page WHERE title = $1", [req.params.articleName], function (err, result) {
+    if (err) {
+        res.status(500).send(err.toString());
+    } else {
+        if (result.rows.length === 0) {
+            res.status(404).send('Article not found');
+        } else {
+            var articleData = result.rows[0];
+            res.send(createTemplate(articleData));
+        }
+    }
+  });
+});
+
 app.get('/ui/:fileName', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', req.params.fileName));
 });
