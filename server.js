bb0b3fde2a0711fec69e14d8d5f97fbbc4b5b9a6
Yaswanth <yaswanthk722@gmail.com>
2016-11-20 13:53:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ app.post('/create-user', function (req, res) {
       if (err) {
           res.status(500).send(err.toString());
       } else {
-          res.send('User successfully created: ' + username);
+          res.send('User successfully created: '+ username);
       }
    });
 });
