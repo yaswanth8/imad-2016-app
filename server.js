d2a43e86703c610a225662579a7891b85096bede
Yaswanth <yaswanthk722@gmail.com>
2016-11-22 21:56:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -220,6 +220,13 @@ app.post('/submit-comment/:articleName', function (req, res) {
         res.status(403).send('Only logged in users can comment');
     }
 });
+// count
+var counter = 0;
+app.get('/counter',function(req,res){
+    counter = counter+1;
+   res.send(counter.toString()); 
+});
+
 
 app.get('/articles/:articleName', function (req, res) {
   // SELECT * FROM article WHERE title = '\'; DELETE WHERE a = \'asdf'
