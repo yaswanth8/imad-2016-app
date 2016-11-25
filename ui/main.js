ffefcdee07c98749d6594744d87847cc9b9d028d
Yaswanth <yaswanthk722@gmail.com>
2016-11-25 12:54:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -136,6 +136,7 @@ function loadLogin () {
                 loadLoggedInUser(this.responseText);
             } else {
                 loadLoginForm();
+                 loadArticleForm();
             }
         }
     };
